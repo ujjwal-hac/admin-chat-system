c7b30c05d1ed84bcfdac63cd2a5efcbea2a7d350
ujjwal-hac <oldidmy081@gmail.com>
2026-03-26 22:49:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -56,3 +56,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
